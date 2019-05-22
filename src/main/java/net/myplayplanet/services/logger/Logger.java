8c1d579451011f8cc4f7a3e646b3ffb5ce004ee6
McTNTCrafter // luca <luca.guettinger@gmail.com>
2019-05-22 19:09:49 +0200

    Update Logger.java

--- src/main/java/net/myplayplanet/services/logger/Logger.java
+++ src/main/java/net/myplayplanet/services/logger/Logger.java
@@ -11,58 +11,55 @@ import java.util.concurrent.Executors;
 public class Logger {
     @NonNull
     private org.slf4j.Logger logger;
-    private static ExecutorService service = Executors.newFixedThreadPool(5);
-
-    public synchronized void synchLog(Throwable ex, LogLevel level, String message, Object... args) {
-        LogEntry logEntry = new LogEntry(ex, level, message, logger, Calendar.getInstance().getTime(), args);
-
-        Log.getSink().save(logEntry);
-
-        if (ex == null) {
-            switch (level) {
-                case DEBUG:
-                    logger.debug(logEntry.getMessageAfterFieldExtraction(), args);
-                    break;
-                case INFO:
-                    logger.info(logEntry.getMessageAfterFieldExtraction(), args);
-                    break;
-                case WARNING:
-                    logger.warn(logEntry.getMessageAfterFieldExtraction(), args);
-                    break;
-                case ERROR:
-                    logger.error(logEntry.getMessageAfterFieldExtraction(), args);
-                    break;
-            }
-        } else {
-            switch (level) {
-                case DEBUG:
-                    logger.debug(logEntry.getMessageAfterFieldExtraction(), ex);
-                    logger.debug(logEntry.getMessageAfterFieldExtraction(), args);
-                    break;
-                case INFO:
-                    logger.info(logEntry.getMessageAfterFieldExtraction(), ex);
-                    logger.info(logEntry.getMessageAfterFieldExtraction(), args);
-                    break;
-                case WARNING:
-                    logger.warn(logEntry.getMessageAfterFieldExtraction(), ex);
-                    logger.warn(logEntry.getMessageAfterFieldExtraction(), args);
-                    break;
-                case ERROR:
-                    logger.error(logEntry.getMessageAfterFieldExtraction(), ex);
-                    logger.error(logEntry.getMessageAfterFieldExtraction(), args);
-                    break;
-            }
-        }
-    }
+    private static ExecutorService executorService = Executors.newFixedThreadPool(1);
 
     public void log(Throwable ex, LogLevel level, String message, Object... args) {
-        service.submit(() -> synchLog(ex, level, message, args));
+        executorService.submit(() -> {
+            LogEntry logEntry = new LogEntry(ex, level, message, logger, Calendar.getInstance().getTime(), args);
+
+            Log.getSink().save(logEntry);
+
+            if (ex == null) {
+                switch (level) {
+                    case DEBUG:
+                        logger.debug(logEntry.getMessageAfterFieldExtraction(), args);
+                        break;
+                    case INFO:
+                        logger.info(logEntry.getMessageAfterFieldExtraction(), args);
+                        break;
+                    case WARNING:
+                        logger.warn(logEntry.getMessageAfterFieldExtraction(), args);
+                        break;
+                    case ERROR:
+                        logger.error(logEntry.getMessageAfterFieldExtraction(), args);
+                        break;
+                }
+            } else {
+                switch (level) {
+                    case DEBUG:
+                        logger.debug(logEntry.getMessageAfterFieldExtraction(), ex);
+                        logger.debug(logEntry.getMessageAfterFieldExtraction(), args);
+                        break;
+                    case INFO:
+                        logger.info(logEntry.getMessageAfterFieldExtraction(), ex);
+                        logger.info(logEntry.getMessageAfterFieldExtraction(), args);
+                        break;
+                    case WARNING:
+                        logger.warn(logEntry.getMessageAfterFieldExtraction(), ex);
+                        logger.warn(logEntry.getMessageAfterFieldExtraction(), args);
+                        break;
+                    case ERROR:
+                        logger.error(logEntry.getMessageAfterFieldExtraction(), ex);
+                        logger.error(logEntry.getMessageAfterFieldExtraction(), args);
+                        break;
+                }
+            }
+        });
     }
 
     public void log(LogLevel level, String message, Object... args) {
         this.log(null, level, message, args);
     }
-
 
     public void debug(Throwable ex, String message, Object... args) {
         log(ex, LogLevel.DEBUG, message, args);

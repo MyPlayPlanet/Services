8fb037aee040ec0dff1062fd331a7389b7feb6d6
Luca Güttinger <38906805+Luca-Guettinger@users.noreply.github.com>
2020-12-25 18:09:00 +0100

    fix maven dependecy

--- src/test/java/net/myplayplanet/service/junit/test/config/ConfigServiceTest.java
+++ src/test/java/net/myplayplanet/service/junit/test/config/ConfigServiceTest.java
@@ -1,7 +1,6 @@
 package net.myplayplanet.service.junit.test.config;
 
-import lombok.extern.slf4j.Slf4j;
-import net.myplayplanet.services.cluster.JavaServiceCluster;
+import net.myplayplanet.services.api.IServiceCluster;
 import net.myplayplanet.services.cluster.JavaClusterBuilder;
 import net.myplayplanet.services.config.ConfigService;
 import net.myplayplanet.services.config.api.IConfigManager;
@@ -14,28 +13,23 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.Properties;
 
-@Slf4j
 public class ConfigServiceTest {
-    private static JavaServiceCluster cluster;
+    private static IServiceCluster cluster;
 
     @BeforeAll
     public static void beforeAll() throws BadSetupException {
-        cluster = (JavaServiceCluster) new JavaClusterBuilder().withConfig().mock(true, "").build();
+        cluster = new JavaClusterBuilder().withConfig().mock(true, "").build();
         cluster.startup();
     }
 
     @AfterAll
-    public static void afterAll() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public static void afterAll() throws InterruptedException {
+        Thread.sleep(1000);
         cluster.shutdownCluster();
     }
 
     @Test
-    public void create_add_and_get_setting_test() {
+    public void create_add_and_get_setting_test() throws IOException {
         //Arrange
         String fileName = "example";
 
@@ -44,11 +38,8 @@ public class ConfigServiceTest {
         IConfigManager configManager = cluster.get(ConfigService.class).getConfigManager();
 
         //Act
-        try {
-            configManager.createSettingWithProperties(fileName, properties);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        configManager.createSettingWithProperties(fileName, properties);
+
         String result = configManager.getProperty(fileName, "key");
 
         //Assert

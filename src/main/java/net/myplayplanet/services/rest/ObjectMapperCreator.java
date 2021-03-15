81ffa6edbceffddea4f7e270ede4f253fed4a661
Martin Panzer <postremus1996@googlemail.com>
2021-03-15 22:25:31 +0100

    Set classloader for jackson By default,. jackson uses Thread.currentThread().getContextClassLoader() as classloader, which is not defined correctly for bukkit plugins

--- src/main/java/net/myplayplanet/services/rest/ObjectMapperCreator.java
+++ src/main/java/net/myplayplanet/services/rest/ObjectMapperCreator.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ObjectMapperCreator {
@@ -11,6 +12,7 @@ public class ObjectMapperCreator {
 
     static {
         INSTANCE = new ObjectMapper();
+        INSTANCE.setTypeFactory(TypeFactory.defaultInstance().withClassLoader(ObjectMapperCreator.class.getClassLoader()));
         INSTANCE.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         INSTANCE.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
         INSTANCE.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
